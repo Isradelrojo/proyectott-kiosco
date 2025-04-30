ffda01a92714b56b64b7eacd9303f3d841fd29a3
Isradelrojo <isradelrojo@gmail.com>
2025-04-30 20:51:48 -0300

    first-commit

---
+++ js/index.js
@@ -0,0 +1,42 @@
+/*const body = document.querySelector("body");
+
+const titulo = document.createElement("h1");
+titulo.textContent = "Soy un titulo generado por javascript.";
+titulo.classList.add("titulo");
+body.appendChild(titulo);*/
+
+const productos = [
+  {
+    id: "Abrigo_01",
+    titulo: "RRopa de abrigo",
+    imagen: "media/abrigo.jpg",
+    categoria: {
+        nombre: "Abrigo",
+        id: "Abrigos",  
+
+    }, 
+    precio: 20000
+  },
+    {
+        id: "Abrigo_02",
+        titulo: "RRopa de abrigo",
+        imagen: "media/abrigo.jpg",
+        categoria: {
+            nombre: "Abrigo",
+            id: "Abrigos",  
+    
+        }, 
+        precio: 20000
+    },
+    {
+        id: "Abrigo_03",
+        titulo: "RRopa de abrigo",
+        imagen: "media/abrigo.jpg",
+        categoria: {
+            nombre: "Abrigo",
+            id: "Abrigos",  
+    
+        }, 
+        precio: 20000
+    }
+]
